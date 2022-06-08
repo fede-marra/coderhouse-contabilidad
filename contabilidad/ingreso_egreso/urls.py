330cfd558f69278b7498e232c21bb45f283b096d
Facundo Fierro <facu.fierro@gmail.com>
2022-06-08 13:17:55 -0300

    agregue el crud personas con vistas y urls. falta poblar templates

--- contabilidad/ingreso_egreso/urls.py
+++ contabilidad/ingreso_egreso/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from ingreso_egreso import views
 
 urlpatterns = [
-    path('', views.busqueda, name="busqueda"),
-    path('ingresos/', views.ingreso, name="ingreso"),
-    path('egresos/', views.egreso, name="egreso"),
-    path('tiposEgresos/', views.tipoEgreso, name='tiposEgresos'),
-    path('personas/', views.persona, name='personas'),
-    path('mostrarPersonas/', views.mostrarPersonas, name='mostrarPersonas'),
-    path('mostarEgresos/', views.mostrarTiposEgresos, name='mostrarEgresos'),
-    path('busqueda/', views.busqueda, name='busqueda'),
+    # path('', views.busqueda, name="busqueda"),
+    path('', views.PersonaList.as_view(), name="persona_list"),
+    path('persona/<int:pk>/', views.PersonaDetail.as_view(), name="persona_detail"),
+    path('persona/new/', views.PersonaCreate.as_view(), name="persona_new"),
+    path('persona/<int:pk>/edit/', views.PersonaUpdate.as_view(), name="persona_edit"),
+    path('persona/<int:pk>/delete/', views.PersonaDelete.as_view(), name="persona_delete"),
+    # path('ingresos/', views.ingreso, name="ingreso"),
+    # path('egresos/', views.egreso, name="egreso"),
+    # path('tiposEgresos/', views.tipoEgreso, name='tiposEgresos'),
+    # path('personas/', views.persona, name='personas'),
+    # path('mostrarPersonas/', views.mostrarPersonas, name='mostrarPersonas'),
+    # path('mostarEgresos/', views.mostrarTiposEgresos, name='mostrarEgresos'),
+    # path('busqueda/', views.busqueda, name='busqueda'),
 ]
