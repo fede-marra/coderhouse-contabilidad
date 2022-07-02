1439a78b2fa7d47539dc0291ec8479cccd3c334a
Facundo Fierro <facu.fierro@gmail.com>
2022-07-02 15:24:47 -0300

    hotfix

--- src/main/settings.py
+++ src/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ndnr)785eh0yp%ly9hzrgd%h912!jfodfkplxy(v8=!wy&*ckz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "localhost"]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "localhost", '127.0.0.1']
 
 
 # Application definition
@@ -134,7 +134,7 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
