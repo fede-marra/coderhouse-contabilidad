03b1d1c606be421e715321e23b0ea743ac42cac0
Facundo Fierro <facu.fierro@gmail.com>
2022-07-02 14:51:11 -0300

    hotfix

--- src/main/settings.py
+++ src/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ndnr)785eh0yp%ly9hzrgd%h912!jfodfkplxy(v8=!wy&*ckz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "localhost"]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "localhost", '127.0.0.1']
 
 
 # Application definition
